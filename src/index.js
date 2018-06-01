645c9eb27716cc181cdecbb7fdbc84c2e1351f63
Clay Roach <clay@j9tech.com>
2018-06-01 16:10:30 -0400

    Adding example nested arrays issue

--- src/index.js
+++ src/index.js
@@ -5,20 +5,40 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import schema from './schema.json';
-import uischema from './uischema.json';
+import schemaworks from './schema-works.json';
+import uischemaworks from './uischema-works.json';
+import schemabroken from './schema-broken.json';
+import uischemabroken from './uischema-broken.json';
 import { Actions, jsonformsReducer } from '@jsonforms/core';
 import { materialFields, materialRenderers } from '@jsonforms/material-renderers';
 import RatingControl from './RatingControl';
 import ratingControlTester from './ratingControlTester'
 
-const data = {
-  name: 'Send email to Adrian',
-  description: 'Confirm if you have passed the subject\nHereby ...',
-  done: true,
-  recurrence: 'Daily',
-  rating: 3,
-};
+const works = {
+  "firstarray": {
+    "objectarrayofstrings": {
+      "choices": [
+        "CHOICE_STRING_1",
+        "CHOICE_STRING_2",
+        "CHOICE_STRING_3"
+      ]
+    }
+  }
+}
+
+const broken = {
+  "firstarray": [
+    {
+      "objectarrayofstrings": {
+        "choices": [
+          "CHOICE_STRING_1",
+          "CHOICE_STRING_2",
+          "CHOICE_STRING_3"
+        ]
+      }
+    }
+  ]
+}
 
 const store = createStore(
   combineReducers({ jsonforms: jsonformsReducer() }),
@@ -30,7 +50,8 @@ const store = createStore(
   }
 );
 
-store.dispatch(Actions.init(data, schema, uischema));
+store.dispatch(Actions.init(broken, schemabroken, uischemabroken));
+//store.dispatch(Actions.init(works, schemaworks, uischemaworks));
 
 
 // Uncomment this line (and respective import) to register our custom renderer
